07ea0e986a889fe48c3829fff11dbc2b1a930c05
Fang Sun <fang.sun@gemalto.com>
2021-08-11 19:33:15 -0400

    Initial Commit

---
+++ Africa Wild Gallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa Wild Gallery
+//
+//  Created by Fang Sun on 2021-08-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
